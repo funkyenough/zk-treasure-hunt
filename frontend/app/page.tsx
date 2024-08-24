6f9c7039c052981972e12beed3d15889f6739751
funkyenough <14842981+funkyenough@users.noreply.github.com>
2024-08-24 16:06:06 +0900

    add frontend and other stuff

---
+++ frontend/app/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function page() {
+  console.log(process.env.GOOGLE_MAP_API_KEY);
+
+  return (
+    <div>
+      <iframe
+        width="600"
+        height="450"
+        // style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAP_API_KEY}&q=Space+Needle,Seattle+WA"
+      ></iframe>
+    </div>
+  );
+}
+
+export default page;
