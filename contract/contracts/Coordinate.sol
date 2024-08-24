d1988f29b0f2cb52796905834d177015123c74aa
漆垣皓大 <kota@urushikakishirodainoMacBook-Pro.local>
2024-08-24 18:58:18 +0900

    contract revice

--- contract/contracts/Coordinate.sol
+++ contract/contracts/Coordinate.sol
@@ -27,7 +27,7 @@ contract zkTreasure {
 
     uint256 public nextGameId;
     uint256 public nextCoordinateId;
-    address public immutable owner;
+    address public owner;
     uint256 public entranceFee;
 
     constructor() {
@@ -59,24 +59,24 @@ contract zkTreasure {
     function createGame(string memory _name, string memory _description, uint256 _duration) external onlyOwner () {
         // Create a new game
         uint256 gameId = nextGameId;
-        games[gameId] = Game(_name, _description, gameId);
-        games[gameId].duration = _duration;
+        Game memory newGame = Game(_name, _description, 0, _duration, new address[](0), new Coordinate[](0), false, address(0), 0, 0 ether);
+        games[gameId] = newGame;
         nextGameId++;
     }   
 
-    function startGame(uint256 _gameId) external onlyOwner checkGameId () { // Start the game
+    function startGame(uint256 _gameId) external onlyOwner () { // Start the game
         assert(games[_gameId].isOver == false); // The game should not have started
         assert(games[_gameId].startAt == 0);
         games[_gameId].startAt = block.timestamp;
     }
 
-    function finishGame(uint256 _gameId) external checkGameId() { // Finish the game
+    function finishGame(uint256 _gameId) external { // Finish the game
         assert(games[_gameId].isOver == false);
         assert(games[_gameId].startAt + games[_gameId].duration <= block.timestamp);
         games[_gameId].isOver = true;
     }
 
-    function createCoordinate(uint256 _gameId, uint256 _x, uint256 _y) external checkGameId { // Create a new coordinate  
+    function createCoordinate(uint256 _gameId, uint256 _x, uint256 _y) external  { // Create a new coordinate  
         assert(games[_gameId].isOver == false); // The game should be ongoing
         assert(games[_gameId].startAt != 0);
         uint256 coordinateId = nextCoordinateId;
@@ -86,21 +86,21 @@ contract zkTreasure {
         nextCoordinateId++;
     }
 
-    function getCoordinate(uint256 _gameId) public view checkGameId returns (Coordinate[] memory) { // Get all coordinates of a game
+    function getCoordinate(uint256 _gameId) public view returns (Coordinate[] memory) { // Get all coordinates of a game
         return games[_gameId].coordinates;
     }
 
-    function getGame(uint256 _gameId) public view checkGameId returns (Game memory) { // Get a game
+    function getGame(uint256 _gameId) public view returns (Game memory) { // Get a game
         return games[_gameId];
     }
  
-    function deposit(uint _gameId) external checkGameId payable { // Deposit the entrance fee
+    function deposit(uint _gameId) external payable { // Deposit the entrance fee
         require(msg.value >= entranceFee, "The amount shoud be more than entranceFee."); 
         games[_gameId].players.push(msg.sender);
         games[_gameId].totalDeposit += msg.value;
     }
 
-    function setWinner(uint256 _gameId) external onlyOwner checkGameId() { // Set the winner
+    function setWinner(uint256 _gameId) external onlyOwner () { // Set the winner
         // Here we compute the winner
         assert(games[_gameId].isOver == true); // The game should be over
         //fraudProofCheck();
@@ -108,13 +108,18 @@ contract zkTreasure {
     }
 
     function payment(uint256 _gameId) public onlyOwner payable { // Send rewards to the winner
-        (bool sent, bytes memory data) = games[_gameId].closestPlayer.call{value: games[_gameId].totalDeposit}("");
-        require(sent, "Failed to send Rewards");
+        (bool success, ) = games[_gameId].closestPlayer.call{value: games[_gameId].totalDeposit}("");
+        require(success, "Failed to send Rewards");
     }
 
-    function fraudProofCheck(address _player, uint256 _gameId) external { // Check if a player is cheating
+    function fraudProofCheck(uint256 _gameId) external { // Check if a player is cheating
         // Here we check if the player is the closest to the treasure
         uint256 x = games[_gameId].coordinates[games[_gameId].coordinates.length - 1].x;
         uint256 y = games[_gameId].coordinates[games[_gameId].coordinates.length - 1].y;
+        uint256 distance = games[_gameId].closestDistance;
+        if(x^2+y^2 < distance^2) {
+            games[_gameId].closestPlayer = msg.sender;
+            games[_gameId].closestDistance = x^2+y^2;
+        }
     }
 }
